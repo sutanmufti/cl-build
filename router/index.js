bb9135c9aee524d1769b3c0a8c22527c099ade8f
sutan mufti <upsutan@gmail.com>
2025-03-13 22:22:17 +0000

    yes

--- router/index.js
+++ router/index.js
@@ -5,7 +5,8 @@ const router = express.Router()
 
 router.get('/', (req, res)=>{
 
-    res.json({message: 'hello from router. Now with continuous integration. this is deployed automatically.'})
+    res.json({message: 'hello from router. Now with continuous integration. this is deployed automatically. Now without https 
+endpoints default.'})
 })
 
 
